91f72819c9dd96291d069cf1da93fa3dd9500e64
sashasevryuk <a_lx_sndr_a@mail.ru>
2022-02-10 14:10:46 +0300

    exit modal comleted

--- js/modal_exit.js
+++ js/modal_exit.js
@@ -1,6 +1,7 @@
 var modal = document.getElementById("exitModal");
 var btn = document.getElementById("exitBtn");
 var btn2 = document.getElementById("exitBtn2");
+var close_btn = document.getElementsByClassName("modal_exit_close_btn")[0];
 
 btn.onclick = function() {
     modal.style.display = "block";
@@ -14,4 +15,8 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+close_btn.onclick = function() {
+  modal.style.display = "none";
 }
