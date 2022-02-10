6491dfec5c179a57b22b1d942a198a85dd9bfc7e
sashasevryuk <a_lx_sndr_a@mail.ru>
2022-02-10 16:11:28 +0300

    add second modal start

--- js/modal_start.js
+++ js/modal_start.js
@@ -1,13 +1,26 @@
 var regbtn = document.getElementById("regBtn");
 var regmodal = document.getElementById("registrationModal");
+var regbtn2 = document.getElementById("regBtn2");
+var regmodal2 = document.getElementById("registrationModal2");
+
 regbtn.onclick = function() {
     regmodal.style.display = "block";
 }
+
+regbtn2.onclick = function() {
+    regmodal2.style.display = "block";
+    regmodal.style.display = "none";
+}
+
 window.onclick = function(event) {
     if (event.target == regmodal) {
         regmodal.style.display = "none";
     }
+    if (event.target == regmodal2) {
+        regmodal2.style.display = "none";
+    }
 }
+
 
 
 
