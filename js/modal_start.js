75d41d88ebc6e27b7a4367a7ec464675b8d9e1de
sashasevryuk <a_lx_sndr_a@mail.ru>
2022-02-11 10:04:31 +0300

    avtorization

--- js/modal_start.js
+++ js/modal_start.js
@@ -2,6 +2,10 @@ var regbtn = document.getElementById("regBtn");
 var regmodal = document.getElementById("registrationModal");
 var regbtn2 = document.getElementById("regBtn2");
 var regmodal2 = document.getElementById("registrationModal2");
+var form = document.getElementById("form");
+var avtbtn = document.getElementById("avtBtn");
+var avtmodal = document.getElementById("avtorizationModal");
+
 window.onclick = function(event) {
     if (event.target == regmodal) {
         regmodal.style.display = "none";
@@ -9,16 +13,31 @@ window.onclick = function(event) {
     //if (event.target == regmodal2) {
     //    regmodal2.style.display = "none";
     //}
+    if (event.target == avtmodal) {
+        avtmodal.style.display = "none";
+    }
 }
 regbtn.onclick = function() {
     regmodal.style.display = "block";
 }
 
-//regbtn2.onclick = function() {
-//    regmodal2.style.display = "block";
-//    regmodal.style.display = "none";
-//}
 
+avtbtn.onclick = function() {
+    avtmodal.style.display = "block";
+}
+/*
+regbtn2.onclick = function() {
+    regmodal.style.display = "none";
+    regmodal2.style.display = "block";
+    
+}
+*/
+/*
+form.onsubmit = function() {
+    //regmodal.style.display = "none";
+    regmodal2.style.display = "block";
+}
+*/
 
 
 
