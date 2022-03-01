340cfb3543fd9321e3e5bb2bed1abd1f2adfdfb9
sashasevryuk <a_lx_sndr_a@mail.ru>
2022-03-01 10:37:58 +0300

    forget password

--- js/modal_start.js
+++ js/modal_start.js
@@ -1,55 +1,32 @@
 var regbtn = document.getElementById("regBtn");
 var regmodal = document.getElementById("registrationModal");
-//var regbtn2 = document.getElementById("regBtn2");
-//var regmodal2 = document.getElementById("registrationModal2");
-//var form = document.getElementById("form");
+
 var avtbtn = document.getElementById("avtBtn");
 var avtmodal = document.getElementById("avtorizationModal");
+
+var avt_help = document.getElementById("avt_help");
+var avt_help_modal = document.getElementById("avt_help_modal");
 
 window.onclick = function (event) {
     if (event.target == regmodal) {
         regmodal.style.display = "none";
     }
-    //if (event.target == regmodal2) {
-    //    regmodal2.style.display = "none";
-    //}
     if (event.target == avtmodal) {
         avtmodal.style.display = "none";
     }
+    if (event.target == avt_help_modal) {
+        avt_help_modal.style.display = "none";
+    }
 }
+
 regbtn.onclick = function () {
     regmodal.style.display = "block";
 }
 
-
 avtbtn.onclick = function () {
     avtmodal.style.display = "block";
 }
-/*
-regbtn2.onclick = function() {
-    regmodal.style.display = "none";
-    regmodal2.style.display = "block";
-    
+
+avt_help.onclick = function() {
+    avt_help_modal.style.display = "block";
 }
-*/
-/*
-form.onsubmit = function() {
-    //regmodal.style.display = "none";
-    regmodal2.style.display = "block";
-}
-*/
-
-
-
-
-
-
-
-
-
-/*
-
-close_btn.onclick = function() {
-  modal.style.display = "none";
-}
-*/
